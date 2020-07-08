e671098784984903eee020ef77d6421eb8dc3760
Angela Guardia <angelaguardia20@gmail.com>
2020-07-08 09:34:38 -0700

    Add product breakdown test

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -102,4 +102,23 @@ class ShoppingCartTest < Minitest::Test
 
     assert_equal [product4, product1, product2, product3], @cart.sorted_products_by_quantity
   end
+
+  def test_it_can_breakdown_the_cart_products_by_category
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+    @cart.add_product(product1)
+    @cart.add_product(product2)
+    @cart.add_product(product3)
+    @cart.add_product(product4)
+
+    expected_breakdown = {
+      :meat=>[product2],
+      :paper=> [product1, product3],
+      :produce=> [product4]
+    }
+
+    assert_equal expected_breakdown, @cart.product_breakdown
+  end
 end

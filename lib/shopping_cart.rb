12f23e46c07d85ddfb2561a28cb2e0671db885ec
Angela Guardia <angelaguardia20@gmail.com>
2020-07-08 08:56:44 -0700

    Update products method to accumulate total and test total number of products

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,16 +1,22 @@
 class ShoppingCart
-  attr_reader :name, :capacity, :products
+  attr_reader :name, :capacity, :products, :total_number_of_products
   def initialize(name, capacity)
     @name = name
     @capacity = capacity[0..1].to_i
     @products = []
+    @total_number_of_products = 0
   end
 
   def add_product(product)
     @products << product
+    @total_number_of_products += product.quantity
   end
 
   def details
     {name: @name, capacity: @capacity}
   end
+
+  # def is_full?
+  #   products.length > @capacity
+  # end
 end
--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -37,4 +37,32 @@ class ShoppingCartTest < Minitest::Test
     cart_details = {name: "King Soopers", capacity: 30}
     assert_equal cart_details, @cart.details
   end
+
+  def test_it_know_the_total_number_of_products
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+
+    @cart.add_product(product1)
+    @cart.add_product(product2)
+    @cart.add_product(product3)
+
+    assert_equal 13, @cart.total_number_of_products
+  end
+
+  # def test_cart_knows_when_its_full
+  #   product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+  #   product2 = Product.new(:meat, 'chicken', 4.50, '2')
+  #   product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+  #
+  #   @cart.add_product(product1)
+  #   @cart.add_product(product2)
+  #   @cart.add_product(product3)
+  #
+  #   assert_equal false, @cart.is_full?
+  #
+  #   product4 = Product.new(:produce, 'apples', 0.99, '20')
+  #
+  #   assert_equal true, @cart.is_full?
+  # end
 end

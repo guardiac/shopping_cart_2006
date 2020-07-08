3e292d4f52a42d3337323a20e0f0f7e4685d221e
Angela Guardia <angelaguardia20@gmail.com>
2020-07-08 09:27:36 -0700

    Add products sorted by quantity methods and test

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -27,4 +27,8 @@ class ShoppingCart
   def percentage_occupied
     ((@total_number_of_products.to_f / @capacity) * 100).round(2)
   end
+
+  def sorted_products_by_quantity
+    (@products.sort_by { |product| product.quantity }).reverse
+  end
 end

a8d57dcea263586e7cb137c5fecc432dd212dce4
Angela Guardia <angelaguardia20@gmail.com>
2020-07-08 08:33:23 -0700

    Add initialize method with attr_readers and tests for shopping cart

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,4 +1,7 @@
 class ShoppingCart
+  attr_reader :name, :capacity
   def initialize(name, capacity)
+    @name = name
+    @capacity = capacity[0..1].to_i
   end
 end
--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -4,9 +4,18 @@ require './lib/product'
 require './lib/shopping_cart'
 
 class ShoppingCartTest < Minitest::Test
+  def setup
+    @cart = ShoppingCart.new("King Soopers", "30items")
+  end
   def test_it_exists
-    cart = ShoppingCart.new("King Soopers", "30items")
+    assert_instance_of ShoppingCart, @cart
+  end
 
-    assert_instance_of ShoppingCart, cart
+  def test_it_has_a_name
+    assert_equal "King Soopers", @cart.name
+  end
+
+  def test_it_has_a_capacity
+    assert_equal 30, @cart.capacity
   end
 end

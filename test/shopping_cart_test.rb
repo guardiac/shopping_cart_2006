237ba4ae35f88547a7cb5013dc79ad4eeecaffdd
Angela Guardia <angelaguardia20@gmail.com>
2020-07-08 08:39:18 -0700

    Add add_product test and method

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -22,4 +22,14 @@ class ShoppingCartTest < Minitest::Test
   def test_it_cart_holds_no_products_by_default
     assert_equal [], @cart.products
   end
+
+  def test_it_can_hold_products
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+
+    @cart.add_product(product1)
+    @cart.add_product(product2)
+
+    assert_equal [product1, product2], @cart.products
+  end
 end
